0cbc819fb51a9b5aab14c82135479b7cd0070e56
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-04-18 23:19:32 +0530

    router for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import Products from './Components/Products/Products';
+import Header from './Components/Header/Header';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <Products />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Product" element={<Product />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
