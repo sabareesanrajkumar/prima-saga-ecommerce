ce1507656284cfe2f71b64ec4ab4c00276a5f6f7
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-04-20 02:52:12 +0530

    making production ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,56 @@
 import './App.css';
 import Header from './Components/Header/Header';
 
+import React, { Suspense, lazy } from 'react';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from 'react-router-dom';
-import Home from './Pages/Home';
-import Product from './Pages/Product';
-import About from './Pages/About';
-import ContactUs from './Pages/ContactUs';
-import ProductDetails from './Pages/ProductDetails';
-import Login from './Pages/Login';
-import { CartProvider } from './Components/Cart/CartContext';
 
 import Auth from './Components/Auth/Auth';
 import Profile from './Components/Profile/Profile';
 import { AuthContext } from './Components/Auth/AuthContext';
 import { useContext } from 'react';
 
+const Home = lazy(() => import('./Pages/Home'));
+const About = lazy(() => import('./Pages/About'));
+const Product = lazy(() => import('./Pages/Product'));
+const ContactUs = lazy(() => import('./Pages/ContactUs'));
+const ProductDetails = lazy(() => import('./Pages/ProductDetails'));
+
 function App() {
   const authContext = useContext(AuthContext);
   return (
     <div className="App">
-      <CartProvider>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {authContext.isLoggedIn && (
-              <Route path="/Product" element={<Product />} />
-            )}
-            <Route path="/About" element={<About />} />
-            <Route path="/ContactUs" element={<ContactUs />} />
-            <Route
-              path="/product-detail/:productId"
-              element={<ProductDetails />}
-            />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {authContext.isLoggedIn && (
+            <Route path="/Product" element={<Product />} />
+          )}
+          <Route path="/About" element={<About />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route
+            path="/product-detail/:productId"
+            element={<ProductDetails />}
+          />
 
-            {!authContext.isLoggedIn && (
-              <Route path="/auth" element={<Auth />} />
-            )}
-            {authContext.isLoggedIn && (
-              <Route path="/profile" element={<Profile />} />
-            )}
-            <Route
-              path="*"
-              element={
-                <Navigate to={authContext.isLoggedIn ? '/profile' : '/auth'} />
-              }
-            />
-          </Routes>
-        </Router>
-      </CartProvider>
+          {!authContext.isLoggedIn && <Route path="/auth" element={<Auth />} />}
+          {authContext.isLoggedIn && (
+            <Route path="/profile" element={<Profile />} />
+          )}
+          <Route
+            path="*"
+            element={
+              <Navigate to={authContext.isLoggedIn ? '/profile' : '/auth'} />
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
